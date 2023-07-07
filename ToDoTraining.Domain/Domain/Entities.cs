6e37ba8861690c1c51f115541fc22219ceff29be
ajrcamigla <125139951+ajrcamigla@users.noreply.github.com>
2023-07-07 16:29:32 +0800

    Add Entities and Interface

---
+++ ToDoTraining.Domain/Domain/Entities.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoTraining.Domain.Domain
+{
+    public class Entities
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
